704e8b18a684b7487a9edc51b3a830d549305acb
TheNinza <niks.a3198@gmail.com>
2020-11-29 00:44:15 +0530

    finished styling menu-items and broke home-page into components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.components";
+
+import "./homepage.styles.scss";
+
+const HompPage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HompPage;
