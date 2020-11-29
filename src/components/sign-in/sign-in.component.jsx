b9837c68fa4450b907fcdf6f0bca01283379833f
TheNinza <niks.a3198@gmail.com>
2020-11-29 21:51:01 +0530

    added forminputs and signin page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
+
+const SignIn = () => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserCredentials({ email: "", password: "" });
+  };
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
+  return (
+    <div className="sign-in">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          type="email"
+          name="email"
+          value={userCredentials.email}
+          handleChange={handleChange}
+          required
+          label="email"
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={userCredentials.password}
+          handleChange={handleChange}
+          required
+          label="password"
+        />
+
+        <CustomButton type="submit">Sign In</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
