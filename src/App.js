9f1e09f238e9fb5096916f3ee59599d406595f94
TheNinza <niks.a3198@gmail.com>
2020-12-15 00:56:24 +0530

    added addItem and removeItem functionality to checkout item component

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App({ currentUser, setCurrentUser }) {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route exact pate="/checkout" component={CheckoutPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
         <Route
           exact
           path="/signin"
