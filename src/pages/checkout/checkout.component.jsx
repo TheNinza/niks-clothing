5f25774e74dae40c23680bcab01de5a6bedf5159
TheNinza <niks.a3198@gmail.com>
2020-12-16 19:36:29 +0530

    added checkout feature <currently handled by local backend

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,15 +1,67 @@
-import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({ cartItems, total }) => {
+const stripePromise = loadStripe(
+  "pk_test_51HytdnG6rUbsulVXed5xUVd6pCBjpL2IF615zGIfT3pGgqz8aj31JdDizRJ2d75oi9BJLxcHUpHpeWuRqWuCmaCP00FYfhASVm"
+);
+
+const CheckoutPage = ({ cartItems, total, currentUser }) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order successful");
+      console.log("success");
+      window.alert("order successful");
+    }
+    if (query.get("canceled")) {
+      setMessage("Order canceled");
+      console.log("canceled");
+      window.alert(
+        "order cancelled, if any amount deducted from your bank, it will be refunded"
+      );
+    }
+  }, []);
+
+  const handlePayment = async () => {
+    console.log("payment started");
+    const stripe = await stripePromise;
+    console.log(cartItems);
+
+    const response = await fetch("http://localhost:5000/checkout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        cartItems: cartItems,
+      }),
+    });
+
+    const session = await response.json();
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.log(result);
+    }
+  };
+
+  const history = useHistory();
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -35,6 +87,15 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ₹{total}</span>
       </div>
+      {currentUser ? (
+        <button className="payment-button" onClick={handlePayment}>
+          Pay Now
+        </button>
+      ) : (
+        <CustomButton onClick={() => history.push("/signin")}>
+          Sign In To Proceed For Payment
+        </CustomButton>
+      )}
     </div>
   );
 };
@@ -42,6 +103,7 @@ const CheckoutPage = ({ cartItems, total }) => {
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal,
+  currentUser: selectCurrentUser,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
